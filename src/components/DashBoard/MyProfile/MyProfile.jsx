c969862c81ed5fdbda241c00e7187d9d6d99f83f
Vibrant <sujaform2024@gmail.com>
2025-03-26 17:28:17 +0500

    changes

--- src/components/DashBoard/MyProfile/MyProfile.jsx
+++ src/components/DashBoard/MyProfile/MyProfile.jsx
@@ -1,13 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import {
-  TextField,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  FormHelperText,
-} from "@mui/material";
+import { TextField, FormControl, InputLabel, Select, MenuItem, FormHelperText } from "@mui/material";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
@@ -37,7 +30,6 @@ const MyProfile = () => {
     },
   });
 
-
   const [user, setUserData] = useState(null);
   const fetchUserData = async () => {
     const token = localStorage.getItem("token");
@@ -46,20 +38,16 @@ const MyProfile = () => {
     }
 
     try {
-      const response = await fetch(
-        "https://app.mayfairweightlossclinic.co.uk/api/profile/GetUserData",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      const response = await fetch("https://app.mayfairweightlossclinic.co.uk/api/profile/GetUserData", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
 
       const data = await response.json();
 
       if (response.ok) {
-
         const user = data?.profile?.user;
         if (user) {
           localStorage.setItem("userData", JSON.stringify(data));
@@ -74,7 +62,6 @@ const MyProfile = () => {
       console.error("Error fetching user data:", error);
     }
   };
-
 
   useEffect(() => {
     fetchUserData();
@@ -116,13 +103,11 @@ const MyProfile = () => {
     }
   }, [user, setValue, trigger]);
 
-
-
   const [isLoading, setIsLoading] = useState(false);
   const onSubmit = async (data) => {
     try {
       setIsLoading(true);
-  
+
       const response = await axios.post(
         "https://app.mayfairweightlossclinic.co.uk/api/profile/UpdateUserData",
         {
@@ -139,55 +124,55 @@ const MyProfile = () => {
           },
         }
       );
-  
+
       // ✅ Handle Success
       toast.success(response.data.message || "Profile updated successfully");
       fetchUserData();
     } catch (error) {
       // ✅ Handle Errors Properly
-      const errorMessage =
-        error.response?.data?.message || "Failed to update profile";
+      const errorMessage = error.response?.data?.message || "Failed to update profile";
       toast.error(errorMessage);
     } finally {
       setIsLoading(false);
     }
   };
-  
 
   return (
     <div className="p-6 sm:bg-[#F9FAFB] sm:min-h-screen sm:rounded-md sm:shadow-md my-5 me-5">
       <div className="mb-8">
-        <h2 className="md:text-xl text-lg mb-2 font-semibold">
-          Profile Information
-        </h2>
-        <p className="reg-font text-gray-600 text-left text-sm xl:w-3/4 mt-2">
-          Update your account's profile information and email address.
-        </p>
+        <h2 className="md:text-xl text-lg mb-2 font-semibold">Profile Information</h2>
+        <p className="reg-font text-gray-600 text-left text-sm xl:w-3/4 mt-2">Update your account's profile information and email address.</p>
       </div>
 
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         {/* First Name and Last Name */}
         <div className="grid sm:grid-cols-2 gap-4">
           <div>
-            <TextField
+            <label htmlFor="name" value="First Name" className="mb-1 block font-medium text-sm text-gray-700">
+              First Name
+            </label>
+            <input
               label="First Name"
               variant="standard"
               fullWidth
-              value={watch("firstName") || ""}
               {...register("firstName", { required: "First name is required" })}
               error={!!errors.firstName}
               helperText={errors.firstName?.message}
+              className="border-gray-300 border focus:border-violet-500 focus:ring-violet-500 rounded-md shadow-sm w-full 2xl:mt-1 2xl:block 2xl:w-full px-2.5 py-2 focus-within:outline-violet-500"
             />
           </div>
           <div>
-            <TextField
+            <label htmlFor="name" value="Last Name" className="mb-1 block font-medium text-sm text-gray-700">
+              Last Name
+            </label>
+            <input
               label="Last Name"
               variant="standard"
               fullWidth
-              value={watch("lastName") || ""}
               {...register("lastName", { required: "Last name is required" })}
               error={!!errors.lastName}
               helperText={errors.lastName?.message}
+              className="border-gray-300 border focus:border-violet-500 focus:ring-violet-500 rounded-md shadow-sm w-full 2xl:mt-1 2xl:block 2xl:w-full px-2.5 py-2 focus-within:outline-violet-500"
             />
           </div>
         </div>
@@ -196,33 +181,36 @@ const MyProfile = () => {
         <div className="grid sm:grid-cols-2 gap-4">
           {/* Gender Selection */}
           <FormControl fullWidth variant="standard" error={!!errors.gender}>
-            <InputLabel>Gender</InputLabel>
+            <label htmlFor="name" value="Gender" className="block font-medium text-sm text-gray-700">
+              Gender
+            </label>
             <Select
               value={watch("gender") || ""}
               onChange={(e) => setValue("gender", e.target.value)}
               {...register("gender", { required: "Gender is required" })}
+              className="profile-input border-gray-300 border bg-white focus:border-violet-500 focus:ring-violet-500 rounded-md shadow-sm w-full 2xl:mt-1 2xl:block 2xl:w-full px-2.5 py-1 focus-within:outline-violet-500"
             >
               <MenuItem value="male">Male</MenuItem>
               <MenuItem value="female">Female</MenuItem>
             </Select>
-            {errors.gender && (
-              <FormHelperText>{errors.gender.message}</FormHelperText>
-            )}
+            {errors.gender && <FormHelperText>{errors.gender.message}</FormHelperText>}
           </FormControl>
 
           {/* Date of Birth */}
           <div>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <label htmlFor="name" value="date" className="block font-medium text-sm text-gray-700">
+                Date of Birth
+              </label>
               <Controller
                 name="dateOfBirth"
                 control={control}
                 rules={{ required: "Date of birth is required" }}
                 render={({ field }) => (
                   <DatePicker
-                    label="Date of Birth"
-                    value={
-                      field.value ? dayjs(field.value, "DD-MM-YYYY") : null
-                    }
+                    className="u-profile-datePicker py-1"
+                    label=""
+                    value={field.value ? dayjs(field.value, "DD-MM-YYYY") : null}
                     onChange={(date) => {
                       handleDateChange(date);
                       field.onChange(date);
@@ -231,7 +219,7 @@ const MyProfile = () => {
                     maxDate={today}
                     slotProps={{
                       textField: {
-                        variant: "standard",
+                        variant: "outlined",
                         fullWidth: true,
                         error: !!dobError,
                         helperText: dobError,
@@ -259,20 +247,9 @@ const MyProfile = () => {
                   message: "Phone number should be at least 10 digits",
                 },
               }}
-              render={({ field }) => (
-                <PhoneInput
-                  country="gb"
-                  placeholder="Enter your phone number"
-                  inputStyle={{ width: "100%" }}
-                  {...field}
-                />
-              )}
+              render={({ field }) => <PhoneInput country="gb" placeholder="Enter your phone number" inputStyle={{ width: "100%" }} {...field} />}
             />
-            {errors.phoneNumber && (
-              <p className="text-red-500 text-sm">
-                {errors.phoneNumber.message}
-              </p>
-            )}
+            {errors.phoneNumber && <p className="text-red-500 text-sm">{errors.phoneNumber.message}</p>}
           </div>
 
           {/* Email Field */}
@@ -283,9 +260,7 @@ const MyProfile = () => {
               {/* Helper text */}
             </div>
 
-            <p className="reg-font text-xs mt-2 text-gray-700">
-              ( This email is associated with your account and cannot be changed)
-            </p>
+            <p className="reg-font text-xs mt-2 text-gray-700">( This email is associated with your account and cannot be changed)</p>
           </div>
         </div>
 
@@ -301,8 +276,7 @@ const MyProfile = () => {
               {isLoading ? (
                 <div className="relative">
                   <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="w-5 h-5 border-2 border-t-transparent border-[#ffffff] rounded-full animate-spin"></div>{" "}
-                    {/* Spinner */}
+                    <div className="w-5 h-5 border-2 border-t-transparent border-[#ffffff] rounded-full animate-spin"></div> {/* Spinner */}
                   </div>
                   <span className="opacity-0">Saving...</span>
                 </div>
--- src/components/PaymentFailed/PaymentFailed.jsx
+++ src/components/PaymentFailed/PaymentFailed.jsx
@@ -1,39 +1,40 @@
-import React from 'react';
-import { FaTimesCircle } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { LuBadgeX } from "react-icons/lu";
 
 const PaymentFailed = () => {
-    const Navigate = useNavigate()
+  const Navigate = useNavigate();
 
-    const handleGoBack = () => {
-        Navigate("/")
-    };
+  const handleGoBack = () => {
+    Navigate("/");
+  };
 
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 px-6">
-            <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-8 text-center max-w-md w-full">
-                <FaTimesCircle size={80} className="text-red-600 mx-auto mb-6" />
-                <h1 className="text-2xl font-bold text-gray-800 dark:text-white">
-                    Ohh Sorry!
-
-                </h1>
-                <p className="mt-2 text-gray-600 dark:text-gray-400">
-                    Your Payment has Failed!.
-
-
-                </p>
-
-                <div className="mt-6 flex gap-4 justify-center">
-                    <button
-                        onClick={handleGoBack}
-                        className="bg-[#4565BF] text-white dark:text-gray-900 font-semibold py-2 px-6 rounded-lg transition-all duration-300"
-                    >
-                        Continue to Available Treatments
-                    </button>
-                </div>
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-[#dacfff] dark:bg-gray-900 px-6">
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-8 text-center w-full">
+          <div className="text-center">
+            <div role="status" className="mb-8">
+              <LuBadgeX className="inline w-16 h-16 text-gray-200 dark:text-gray-600 fill-purple-600 mx-auto mb-4" />
+              <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-300 mb-4">Ohh Sorry!</h2>
+              <div className="text-left text-gray-600 dark:text-gray-400 mb-5 xl:w-3/5 xl:mx-auto">
+                <center>
+                  {" "}
+                  <p>Your Payment has Failed!.</p>
+                </center>
+              </div>
             </div>
+            <button
+              onClick={handleGoBack}
+              className="bg-violet-700 hover:bg-violet-600 text-white px-2 sm:px-8 py-2 rounded-md font-medium transition-all duration-150 ease-in"
+            >
+              Continue to Available Treatments
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default PaymentFailed;
