26d762b01a2dee3f86082034476f110e0b7951bf
Waqas Ahmed <waqas.vmi@gmail.com>
2025-04-07 16:07:00 +0500

    Thank ypu page width fix

--- src/components/ThankYou/ThankYou.jsx
+++ src/components/ThankYou/ThankYou.jsx
@@ -86,7 +86,7 @@ const ThankYou = () => {
   // }, []);
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[#dacfff]  px-6">
+    <div className="flex items-center justify-center min-h-screen bg-[#dacfff] px-6 sm:px-44">
       <div className="bg-white  rounded-lg shadow-lg p-8 text-center w-full">
         <div className="text-center">
           <div role="status" className="mb-8">
