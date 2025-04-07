4875d8c0bccad85fbf0f5aec629f4097bfe27e90
shahrukhvmi <sujaform2024@gmail.com>
2025-04-07 19:48:57 +0500

    update

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -12,6 +12,7 @@ import Stepseven from "./formsteps/Stepseven";
 import Stepeight from "./formsteps/Stepeight";
 import Stepper from "./Stepper";
 import Footer from "./Footer";
+import  ApplicationLogo  from "../config/ApplicationLogo";
 const Steps = () => {
   const [hideSidebar, setHideSidebar] = useState(false);
   const [searchParams] = useSearchParams();
@@ -64,12 +65,12 @@ const Steps = () => {
         <div className="consultation-form mx-auto w-full lg:mx-5 lg:w-auto xl:mx-6 xl:w-auto 2xl:mx-auto 2xl:w-[1366px] flex flex-col lg:flex-row font-inter overflow-hidden">
           {/* Left Sidebar */}
           <div
-            className={`left lg:w-[35%] bg-[#160647] mx-3 md:mx-6 lg:mx-0 px-5 py-5 lg:px-6 xl:px-10 lg:py-6 xl:py-10 rounded-tl-xl rounded-tr-xl lg:rounded-tr-none lg:rounded-bl-xl shadow-lg ${
-              hideSidebar ? "hidden" : ""
-            }`}
+            className={`left lg:w-[35%] bg-[#160647] mx-3 md:mx-6 lg:mx-0 px-5 py-5 lg:px-6 xl:px-10 lg:py-6 xl:py-10 rounded-tl-xl rounded-tr-xl lg:rounded-tr-none lg:rounded-bl-xl shadow-lg ${hideSidebar ? "hidden" : ""
+              }`}
           >
             <div className="brightness-0 invert w-32 lg:w-40">
-              <img src="/logo.svg" className="w-36 sm:w-36" alt="Logo" />
+              {/* <img src="/logo.svg" className="w-36 sm:w-36" alt="Logo" /> */}
+              <ApplicationLogo className="w-36 sm:w-36" />
             </div>
             <div className="text-wrapper text-white">
               <h1 className="font-bold text-4xl lg:text-3xl xl:text-4xl 2xl:text-5xl pt-6 leading-tight lg:pe-10">
@@ -87,9 +88,8 @@ const Steps = () => {
 
           {/* Right Side */}
           <div
-            className={`right relative bg-white mx-3 md:mx-6 lg:mx-0 ${
-              hideSidebar ? "w-auto lg:w-full rounded-xl" : "lg:w-[65%] lg:rounded-tr-xl rounded-bl-xl lg:rounded-bl-none rounded-br-xl"
-            }`}
+            className={`right relative bg-white mx-3 md:mx-6 lg:mx-0 ${hideSidebar ? "w-auto lg:w-full rounded-xl" : "lg:w-[65%] lg:rounded-tr-xl rounded-bl-xl lg:rounded-bl-none rounded-br-xl"
+              }`}
           >
             <div className="flex flex-col justify-between h-full">
               <div className={`step-handler-wrapper p-5 xl:p-10 ${hideSidebar ? "" : "overflow-y-auto lg:h-[calc(100vh-250px)]"}`}>
